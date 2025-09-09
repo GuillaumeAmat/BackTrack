e8fefe09ea388100b2b69c1dbbfa20b3e5cb8653
Guillaume AMAT <guillaume@amat.io>
2025-09-10 00:10:09 +0200

    feat: move the vehicle on arrow input

--- game/world/Vehicle.ts
+++ game/world/Vehicle.ts
@@ -21,6 +21,10 @@ export class Vehicle {
     height: VEHICLE_SIZE,
     depth: VEHICLE_SIZE,
     radius: 0.4,
+    minX: -VEHICLE_SIZE,
+    maxX: VEHICLE_SIZE,
+    minY: VEHICLE_SIZE / 2 + OBSTACLE_VEHICLE_SIZE_DIFF,
+    maxY: VEHICLE_SIZE / 2 + OBSTACLE_VEHICLE_SIZE_DIFF + 2 * VEHICLE_SIZE,
   };
 
   #debug: Debug;
@@ -33,15 +37,21 @@ export class Vehicle {
   }
 
   constructor(scene: Scene) {
+    if (!window) {
+      throw new Error('"Vehicle" can only be instanciated in a browser environment.');
+    }
+
     this.#scene = scene;
     this.#debug = Debug.getInstance();
 
     this.createMesh();
     this.setupHelpers();
+
+    window.addEventListener('keyup', (event) => this.#onKeyUp(event));
   }
 
   private createMesh() {
-    const { width, height, radius, depth } = this.#properties;
+    const { width, height, radius, depth, minY } = this.#properties;
 
     this.#mesh = createRoundedPlaneMesh(width, height, radius, {
       extrusionDepth: depth,
@@ -51,7 +61,7 @@ export class Vehicle {
     this.#mesh.castShadow = true;
     this.#mesh.position.x = 0;
     this.#mesh.position.z = 0;
-    this.#mesh.position.y = height / 2 + OBSTACLE_VEHICLE_SIZE_DIFF;
+    this.#mesh.position.y = minY;
 
     this.#scene.add(this.#mesh);
   }
@@ -77,6 +87,23 @@ export class Vehicle {
           this.#debug.save();
         });
       }
+    }
+  }
+
+  #onKeyUp(event: KeyboardEvent) {
+    if (!this.#mesh) {
+      return;
+    }
+    const { width, height, minX, maxX, minY, maxY } = this.#properties;
+
+    if (['ArrowLeft', 'KeyA', 'KeyQ'].includes(event.code)) {
+      this.#mesh.position.x = Math.max(this.#mesh.position.x - width, minX);
+    } else if (['ArrowRight', 'KeyD'].includes(event.code)) {
+      this.#mesh.position.x = Math.min(this.#mesh.position.x + width, maxX);
+    } else if (['ArrowUp', 'KeyW', 'KeyZ'].includes(event.code)) {
+      this.#mesh.position.y = Math.min(this.#mesh.position.y + height, maxY);
+    } else if (['ArrowDown', 'KeyS'].includes(event.code)) {
+      this.#mesh.position.y = Math.max(this.#mesh.position.y - height, minY);
     }
   }
 
